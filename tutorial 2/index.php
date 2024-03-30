c6bbaf8c6007ef0482e01fff479d0668c0b6eae3
Arvee <arveedurante220@gmail.com>
2024-03-30 09:48:19 +0800

    change .html into .php and add some css

--- tutorial 2/index.php
+++ tutorial 2/index.php
@@ -5,11 +5,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Tutorial 2</title>
   <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="./syle.css">
   <script>
-    
+
   </script>
 </head>
 <body>
-  
+  <div class="container">
+    
+  </div>
+  <button>View more convo</button>
 </body>
 </html>
